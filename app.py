fc52d9032034835465854a4db69e19022f52720d
Amir <amirsahrani1@gmail.com>
2022-10-23 14:40:28 +0200

    Reset directory

--- app.py
+++ app.py
@@ -1,21 +1,56 @@
-from  dash import Dash, html, dcc
+from  dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+dx = pd.DataFrame(columns =
+      ['AVG(retail_and_recreation_percent_change_from_baseline)',
+       'AVG(grocery_and_pharmacy_percent_change_from_baseline)',
+       'AVG(parks_percent_change_from_baseline)',
+       'AVG(transit_stations_percent_change_from_baseline)',
+       'AVG(workplaces_percent_change_from_baseline)',
+       'AVG(residential_percent_change_from_baseline)'])
+category_dropdown = dcc.Dropdown([
+{"label": "Retail and Recreation", "value" : dx.columns[0]},
+{"label": "Grocery and Pharmacy" , "value" : dx.columns[1]},
+{"label": "Parks"                , "value" : dx.columns[2]},
+{"label": "Transit stations"     , "value" : dx.columns[3]},
+{"label": "Workplaces"           , "value" : dx.columns[4]},
+{"label": "Residential areas"    , "value" : dx.columns[5]}]
+, value = dx.columns[0],id = "category-selector")
 
-fig = px.bar(df, x = "Fruit", y= "Amount", color = "City", barmode = "group")
 app.layout = html.Div(children= [
-    html.H1(children = "First app"),
+    html.H1(children = "Mobility data throughout a Pandemic"),
     html.Div(children= '''
-    graph of amount of fruit consumption per city'''),
-    dcc.Graph(id='example-graph',figure=fig),
+    Time spent in Retail and recreational facilities compared to baseline'''),
+    category_dropdown,
+    html.Div(id="category-container"),
+    dcc.Graph(id='Map-category', figure = {}),
+    # dcc.Loading(dcc.Graph(id="graph"), type="cube")
     
 ])
+
+@app.callback(
+    [Output('category-container', 'children'),
+    Output('Map-category', component_property= 'figure')],
+    Input('category-selector', 'value')
+)
+
+def update_output(value):
+    f = pd.read_parquet("Averages all categories", columns = ["country_region_code", "date", str(value)])
+    df = pd.DataFrame(f)
+    container = f"Currently looking at {value} data"
+    fig = px.scatter_geo(df, locations="country_region_code",
+                        hover_name="country_region_code", size= value,
+                        animation_frame="date",
+                        animation_group="country_region_code",
+                        projection="natural earth",
+                        template = 'plotly_dark',
+                        redraw = False)
+    fig.update_layout(transition = {'duration': 1})
+    return container, fig
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
