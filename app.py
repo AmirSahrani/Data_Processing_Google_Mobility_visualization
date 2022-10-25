74a56422fc4f51f950ca0ca74e1345c3cf1dddde
Amir <amirsahrani1@gmail.com>
2022-10-25 16:30:22 +0200

    Added previous changes to app, testing time color

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from  dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
+f = pd.read_csv("GDP data.csv", encoding= "latin")
+dp = pd.DataFrame(f)
+dict2020 = {}
+dict2021 = {}
+dict2022 = {}
+for i in dp.iterrows():
+    dict2020[i[1]["Country"]] =  i[1]["2020"]
+    dict2021[i[1]["Country"]] =  i[1]["2021"]
+    dict2022[i[1]["Country"]] =  i[1]["2020.1"]
+
 app = Dash(__name__)
 dx = [ 'AVG(retail_and_recreation_percent_change_from_baseline)',
        'AVG(grocery_and_pharmacy_percent_change_from_baseline)',
@@ -38,19 +48,24 @@ def update_output(value):
     container = f"Currently looking at {value} data"
 
 
-    f = pd.read_parquet("Averages all categories", columns = ["country_region_code", "date", str(value)])
+    f = pd.read_parquet("Averages all categories", columns = ["Country", "date", str(value)])
     df = pd.DataFrame(f)
-    df['date'] = pd.to_datetime(df['date'])
-    df1 = df.groupby(["country_region_code",pd.Grouper(freq="w",key = "date")]).mean()[value].reset_index()
-    fig = px.scatter_geo(df1, locations= "country_region_code",
-                        hover_name="country_region_code", size= value,
-                        animation_frame=df1.date.astype(str),
+    df['date'] = pd.to_datetime(df['date'], )
+    Final_df = df.groupby(["Country",pd.Grouper(freq="w",key = "date")]).mean()[value].reset_index()
+    Final_df["GDP 2020"]= Final_df["Country"].map(dict2020)
+    Final_df["GDP 2021"]= Final_df["Country"].map(dict2021)
+    Final_df["GDP 2022"]= Final_df["Country"].map(dict2022)
+
+    fig = px.scatter_geo(Final_df, locations= "Country", locationmode= "country names",
+                        hover_name="Country", size= value,
+                        animation_frame=Final_df.date.astype(str),
+                        color = "GDP 2020",
                         projection="natural earth",
                         template = 'plotly_dark')
 
     fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
     fig.update_layout(height = 600)
-
+    fig.update_layout(title = fig.layout.sliders[0].steps[0].label)
     return container, fig
 
 if __name__ == '__main__':
